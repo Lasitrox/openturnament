590d3c5a023913e52a137f0e169f1259529e3bb6
Lasitrox <lasitrox@t-online.de>
2025-12-27 20:22:09 +0100

    Fixes
    subprocess not finding tailwind probably caused by pycharm on linux
    tailwind 4.0 bump

--- src/main.py
+++ src/main.py
@@ -25,10 +25,22 @@ async def lifespan(app: FastAPI):
             "-o",
             str(settings.STATIC_DIR / "css" / "main.css"),
         ])
+    except FileNotFoundError:
+        try:
+            subprocess.run([
+                "./.venv/bin/tailwindcss",
+                "-i",
+                str(settings.STATIC_DIR / "src" / "tw.css"),
+                "-o",
+                str(settings.STATIC_DIR / "css" / "main.css"),
+            ])
+        except Exception as e:
+            print(f"Error running tailwindcss: {e}")
     except Exception as e:
         print(f"Error running tailwindcss: {e}")
 
     yield
+
 
 def get_app() -> FastAPI:
     """Create a FastAPI app with the specified settings."""
